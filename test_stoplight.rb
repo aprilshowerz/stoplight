dc4f868d56e560223b3543049868e8be5996e46e
April Blair <a.m.blair@eagle.clarion.edu>
2017-02-13 09:14:57 -0500

    tests

--- test_stoplight.rb
+++ test_stoplight.rb
@@ -7,8 +7,21 @@ def test_one_returns_one()
 	assert_equal(1, stoplight_result(1))
 end
 
+def test_pass_1_to_stoplight_returns_1
+	assert_equal(1, stoplight_result(1))
+end
 
-
-
+def test_two_returns_two()
+	assert_equal(2, stoplight_result(2))
+end
+def test_three_returns_Yellow()
+	assert_equal('Yellow', stoplight_result(3))
+end
+def test_divisible_by_three_returns_Red()
+	assert_equal('Red', stoplight_result(9))
+end
+def test_divisible_by_three_returns_Green()
+	assert_equal('Green', stoplight_result(15))
+end
 
 end
